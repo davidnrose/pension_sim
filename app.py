5ec76ccd9d16df734f6612785b9e7c8bb19427d2
David <davidnathanrose@gmail.com>
2026-02-20 08:44:07 +0000

    adjusted plotly graph width parameter

--- app.py
+++ app.py
@@ -117,4 +117,4 @@ with col2:
         legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1)
     )
 
-    st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig, width="stretch")
